48ba060bde303b2a97474bc111d799dd14733fd8
Richard Kelley <richard.kelley@gmail.com>
2024-06-19 11:02:28 -0700

    Started MatchResults, Match.

--- lm_tournament_eval/api/match.py
+++ lm_tournament_eval/api/match.py
@@ -1,5 +1,7 @@
 # a match is a sampled subset of points from a dataset.
 
+from lm_tournament_eval.api.task import TaskConfig, Task
+
 from dataclasses import dataclass, field
 from typing import Optional, List
 
@@ -8,15 +10,23 @@ class MatchResult:
     """
     A record of the results of a match between two models.
     """
+    model0_name: str
     model1_name: str
-    model2_name: str
+    model0_old_elo: float
     model1_old_elo: float
-    model2_old_elo: float
+    model0_new_elo: float
     model1_new_elo: float
-    model2_new_elo: float
-    task: str
-    task_split: Optional[str]
-    task_indices: List[int]
+    task_config : TaskConfig
+
+    task_indices : List[int] = field(
+        default=[],
+        metadata={"doc" : "Indices from the task that make up the match"}
+    )
+
+    match_points : List[int] = field(
+        default=[],
+        metadata={"doc" : "Array of winner for each 'point' - model 0 or model 1."}
+    )
 
 class Match:
     def __init__(self):
