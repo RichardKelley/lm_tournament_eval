4eaa74cc312164e80212b6b3d7e4f9efc505ad69
Richard Kelley <richard.kelley@gmail.com>
2024-06-19 11:02:08 -0700

    Stub for Tournament. Probably should be an interface...

--- lm_tournament_eval/api/tournament.py
+++ lm_tournament_eval/api/tournament.py
@@ -1,8 +1,30 @@
 # this is a collection of matches, models, and a schedule of "play"
 
+from dataclasses import dataclass, field
+
+@dataclass
+class TournamentConfig:
+    name : str
+    model1_name : str
+    model2_name : str
+    task_name : str
+    rounds : int
+
+
 class Tournament:
-    def __init__(self):
+    def __init__(self, config : TournamentConfig):
         pass
+
+    def run_tournament(self):
+        print("Running tournament!")
+
+        # for number of rounds
+        # create match
+        # run match
+        # record results
+        # update Elo
+
+        return {}
 
 
     
