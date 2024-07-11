79527022da98dd43904028c04c209216b04d01ae
Richard Kelley <richard.kelley@gmail.com>
2024-07-10 23:07:22 -0700

    Started expanding task class.

--- lm_tournament_eval/api/task.py
+++ lm_tournament_eval/api/task.py
@@ -1,6 +1,8 @@
-import datasets
+from datasets import load_dataset
 from dataclasses import dataclass, field
 from typing import Optional, Union
+
+import logging
 
 from .instance import OutputType, Instance
 
@@ -19,20 +21,37 @@ class TaskConfig:
     metric_list : Optional[list] = None
     output_type : OutputType = "multiple_choice"
 
+    def __getitem__(self, item):
+        return getattr(self, item)
+    
+    def __setitem__(self, item, value):
+        setattr(self, item, value)
+
 
 class Task:
     def __init__(self, config) -> None:
         self.config = config
+        if config.task is not None:
+            self._task_name = config.task
 
-    def download(self):
-        dataset = datasets.load_dataset('hellaswag')
-        return dataset
+        self._instances = []
 
-    def create_instances(self, dataset):
-        self.instance_list = []
-        for req in dataset["validation"]:
+        self._download()
+        self._create_instances()
+
+    @property
+    def task_name(self) -> str:
+        return self._task_name
+
+    def _download(self) -> None:
+        logging.log(logging.INFO, f"Downloading dataset {self.task_name}")
+        self._dataset = load_dataset(self.task_name)
+
+    def _create_instances(self) -> None:
+        logging.log(logging.INFO, "Preparing instances for evaluation...")        
+        for req in self._dataset["validation"]:
             for i, ending in enumerate(req["endings"]):
-                self.instance_list.append(Instance(request_type=self.config.output_type,
+                self._instances.append(Instance(request_type=self.config.output_type,
                                                    doc=req,
                                                    arguments=(req['ctx'],ending),
                                                    idx=i
