86d57bdb1864d40dcbcc92af2df05c4da62ab79a
Richard Kelley <richard.kelley@gmail.com>
2024-07-10 23:08:01 -0700

    Setting up tournament evaluation code.

--- lm_tournament_eval/api/tournament.py
+++ lm_tournament_eval/api/tournament.py
@@ -1,7 +1,12 @@
 # this is a collection of matches, models, and a schedule of "play"
 
+import logging
+import time
+
 from dataclasses import dataclass, field
 from .task import Task, TaskConfig
+
+from .tasks import TaskManager
 
 @dataclass
 class TournamentConfig:
@@ -16,13 +21,41 @@ class Tournament:
     def __init__(self, config : TournamentConfig):
         pass
 
-    def run_tournament(self):
-        print("Running tournament!")
-        cfg = TaskConfig()
-        task = Task(cfg)
-        dataset = task.download()
-        task.create_instances(dataset)
+    def tournament_evaluate(
+        self,
+        model0,
+        model1,
+        tasks,
+        gen_kwargs,
+        task_manager,
+        random_seed,
+        numpy_random_seed,
+        torch_random_seed
+    ):
+        logging.info("Running tournament!")
         
+        start_date = time.time()
+
+        # TODO set random seeds
+
+        if tasks is None:
+            tasks = []
+        if len(tasks) == 0:
+            raise ValueError(
+                "No tasks specified or no tasks found."
+            )
+        
+        # TODO set up gen_kwargs
+
+        # TODO handle model initialization. TODO decide if we want to allow strings.
+
+        if task_manager is None:
+            task_manager = TaskManager()
+
+        # TODO setup match parameters
+
+        
+
         # for number of rounds
         # create match
         # run match
