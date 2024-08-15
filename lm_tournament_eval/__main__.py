638b7a1a3db18de904d320ef71eeec7d44f9f296
Duncan Wilson <duncanw@nevada.unr.edu>
2024-08-14 19:52:06 -0700

    make offline tournaments great again

--- lm_tournament_eval/__main__.py
+++ lm_tournament_eval/__main__.py
@@ -4,7 +4,6 @@ import sys
 import logging
 import datetime
 import os
-import csv
 
 from lm_tournament_eval import utils
 from lm_tournament_eval.api.tournament import TournamentConfig, Tournament
@@ -21,10 +20,8 @@ def setup_parser() -> argparse.ArgumentParser:
     parser.add_argument("--model1", "-m1", type=str, help="Name of second competing model.")
     parser.add_argument("--model1_args", type=str, help="Arguments for model 1.")
     parser.add_argument("--tasks", "-t", default=None, type=str, metavar="task1,task2")
-    parser.add_argument("--filter", default='none', type=str)
     parser.add_argument("--num_rounds", default=1, type=int)
     parser.add_argument("--batch_size", "-b", default=1, type=int)
-    parser.add_argument("--gen_kwargs", type=str, default=None, help=("String arguments for model generation on greedy_until tasks, e.g. `temperature=0,top_k=0,top_p=0`."))
     parser.add_argument("--match_size", default=1, type=int)
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--output_path", "-o", type=str, default=".")
@@ -50,13 +47,6 @@ def setup_parser() -> argparse.ArgumentParser:
                         help="File path for first model results.")
     parser.add_argument("--offline_file_1", type=str, default="",
                         help="File path for second model results.")
-    
-    parser.add_argument("--elo_csv_in", type=str, default=None,
-                        help="Path to CSV file with initial ELO scores.")
-    #parser.add_argument("--save_scores", type=bool, default=False,
-    #                    help="Write scores back out to input file.")
-    parser.add_argument("--elo_csv_out", type=str, default=None,
-                        help="Path to CSV file to write updated ELO scores.")
 
     return parser
 
@@ -67,91 +57,9 @@ def run_tournament():
     # handle arguments.
     parser = setup_parser()
     args = parser.parse_args()
-
-    if args.include_path is not None:
-        logging.info(f"Including path: {args.include_path}")
-    task_manager = TaskManager(args.verbosity, include_path=args.include_path)
-
-    if args.tasks is None:
-        logging.error("Need to specify a task to evaluate.")    
-        sys.exit()
-    elif args.tasks == "list":
-        print(task_manager.list_all_tasks())
-        sys.exit()
-    elif args.tasks == "list_groups":
-        print(task_manager.list_all_tasks(list_subtasks=False, list_tags=False))
-        sys.exit()
-    elif args.tasks == "list_tags":
-        print(task_manager.list_all_tasks(list_groups=False, list_subtasks=False))
-        sys.exit()
-    elif args.tasks == "list_subtasks":
-        print(task_manager.list_all_tasks(list_groups=False, list_tags=False))
-        sys.exit()
-    else:
-        if os.path.isdir(args.tasks):
-            import glob
-
-            task_names = []
-            yaml_path = os.path.join(args.tasks, "*.yaml")
-            for yaml_file in glob.glob(yaml_path):
-                config = utils.load_yaml_config(yaml_file)
-                task_names.append(config)
-        else:
-            task_list = args.tasks.split(",")
-            task_names = task_manager.match_tasks(task_list)
-            for task in [task for task in task_list if task not in task_names]:
-                if os.path.isfile(task):
-                    config = utils.load_yaml_config(task)
-                    task_names.append(config)
-            task_missing = [
-                task for task in task_list if task not in task_names and "*" not in task
-            ]  # we don't want errors if a wildcard ("*") task name was used
-
-            if task_missing:
-                missing = ", ".join(task_missing)
-                logging.error(
-                    f"Tasks were not found: {missing}\n"
-                    f"{utils.SPACING}Try `lm-eval --tasks list` for list of available tasks",
-                )
-                raise ValueError(
-                    f"Tasks not found: {missing}. Try `lm-eval --tasks {{list_groups,list_subtasks,list_tags,list}}` to list out all available names for task groupings; only (sub)tasks; tags; or all of the above, or pass '--verbosity DEBUG' to troubleshoot task registration issues."
-                )
-
-    # Respect user's value passed in via CLI, otherwise default to True and add to comma-separated model args
-    if args.trust_remote_code:
-        logging.info(
-            "Passed `--trust_remote_code`, setting environment variable `HF_DATASETS_TRUST_REMOTE_CODE=true`"
-        )
-        # HACK: import datasets and override its HF_DATASETS_TRUST_REMOTE_CODE value internally,
-        # because it's already been determined based on the prior env var before launching our
-        # script--`datasets` gets imported by lm_eval internally before these lines can update the env.
-        import datasets
-
-        datasets.config.HF_DATASETS_TRUST_REMOTE_CODE = True
-
-        args.model_args = args.model_args + ",trust_remote_code=True"
-
-    logging.info(f"Selected Tasks: {task_names}")
-
-    # TODO support caching?
-    #request_caching_args = request_caching_arg_to_dict(
-    #    cache_requests=args.cache_requests
-    #)
-
-    args.tournament_name = "{}-{}-{}".format(datetime.datetime.now(), args.model0, args.model1)
     
     # set up local logger.
     # set up wandb logger.
-
-    initial_elos = {}
-    if args.elo_csv_in is not None:
-        with open(args.elo_csv_in, 'r') as f:
-            reader = csv.reader(f, delimiter=',')
-            for row in reader:
-                model, bpw, elo = row
-                initial_elos.update({(model, bpw): float(elo)})
-
-    logging.info(f"Using initial elo scores {initial_elos}")
 
     if args.offline == True:
         # validate tournament parameters.
@@ -172,6 +80,78 @@ def run_tournament():
         # run tournament evaluator.
         result = tournament.run_tournament()    
     else:
+
+        if args.include_path is not None:
+            logging.info(f"Including path: {args.include_path}")
+        task_manager = TaskManager(args.verbosity, include_path=args.include_path)
+
+        if args.tasks is None:
+            logging.error("Need to specify a task to evaluate.")    
+            sys.exit()
+        elif args.tasks == "list":
+            print(task_manager.list_all_tasks())
+            sys.exit()
+        elif args.tasks == "list_groups":
+            print(task_manager.list_all_tasks(list_subtasks=False, list_tags=False))
+            sys.exit()
+        elif args.tasks == "list_tags":
+            print(task_manager.list_all_tasks(list_groups=False, list_subtasks=False))
+            sys.exit()
+        elif args.tasks == "list_subtasks":
+            print(task_manager.list_all_tasks(list_groups=False, list_tags=False))
+            sys.exit()
+        else:
+            if os.path.isdir(args.tasks):
+                import glob
+
+                task_names = []
+                yaml_path = os.path.join(args.tasks, "*.yaml")
+                for yaml_file in glob.glob(yaml_path):
+                    config = utils.load_yaml_config(yaml_file)
+                    task_names.append(config)
+            else:
+                task_list = args.tasks.split(",")
+                task_names = task_manager.match_tasks(task_list)
+                for task in [task for task in task_list if task not in task_names]:
+                    if os.path.isfile(task):
+                        config = utils.load_yaml_config(task)
+                        task_names.append(config)
+                task_missing = [
+                    task for task in task_list if task not in task_names and "*" not in task
+                ]  # we don't want errors if a wildcard ("*") task name was used
+
+                if task_missing:
+                    missing = ", ".join(task_missing)
+                    logging.error(
+                        f"Tasks were not found: {missing}\n"
+                        f"{utils.SPACING}Try `lm-eval --tasks list` for list of available tasks",
+                    )
+                    raise ValueError(
+                        f"Tasks not found: {missing}. Try `lm-eval --tasks {{list_groups,list_subtasks,list_tags,list}}` to list out all available names for task groupings; only (sub)tasks; tags; or all of the above, or pass '--verbosity DEBUG' to troubleshoot task registration issues."
+                    )
+
+        # Respect user's value passed in via CLI, otherwise default to True and add to comma-separated model args
+        if args.trust_remote_code:
+            logging.info(
+                "Passed `--trust_remote_code`, setting environment variable `HF_DATASETS_TRUST_REMOTE_CODE=true`"
+            )
+            # HACK: import datasets and override its HF_DATASETS_TRUST_REMOTE_CODE value internally,
+            # because it's already been determined based on the prior env var before launching our
+            # script--`datasets` gets imported by lm_eval internally before these lines can update the env.
+            import datasets
+
+            datasets.config.HF_DATASETS_TRUST_REMOTE_CODE = True
+
+            args.model_args = args.model_args + ",trust_remote_code=True"
+
+        logging.info(f"Selected Tasks: {task_names}")
+
+        # TODO support caching?
+        #request_caching_args = request_caching_arg_to_dict(
+        #    cache_requests=args.cache_requests
+        #)
+
+        args.tournament_name = "{}-{}-{}".format(datetime.datetime.now(), args.model0, args.model1)
         # validate tournament parameters.
         cfg = TournamentConfig(name=args.tournament_name,
                               rounds=args.num_rounds,
@@ -181,21 +161,13 @@ def run_tournament():
                               model1_args=args.model1_args,
                               task_names=task_names,
                               batch_size=args.batch_size,
-                              gen_kwargs=args.gen_kwargs,
                               device=args.device,
                               limit=args.limit,
-                              match_size=args.match_size,
-                              cmd_filter=args.filter
+                              match_size=args.match_size
                              )
 
         #create tournament
-        tournament = Tournament(
-            cfg, 
-            task_names, 
-            task_manager, 
-            args.verbosity, 
-            initial_elos,
-            args.elo_csv_out)
+        tournament = Tournament(cfg, task_names, task_manager, args.verbosity)
 
         #logging.info(f"Running tournament {cfg}")
         tournament.run_tournament()
--- lm_tournament_eval/api/elo.py
+++ lm_tournament_eval/api/elo.py
@@ -1,39 +1,33 @@
+import math
 from .match import MatchResult, Match
 from typing import List, Dict
-import logging
-import csv
 
 def argmax(iterable):
     return max(enumerate(iterable), key=lambda x: x[1])[0]
-
+MAX_SCORE_DIFF = 800  # You can adjust this value
+RATING_FLOOR = 100
 class ELO:
-    def __init__(self, model0_key, model1_key, initial_elos=None, elo_out=None):
-
-        if initial_elos is not None:
-            self.initial_elos = initial_elos
-            self.model0_name = model0_key[0]
-            self.model0_bpw = model0_key[1]
-            self.model1_name = model1_key[0]
-            self.model1_bpw = model1_key[1]
-            self.elo_out = elo_out
-            # set the initial scores
-            if model0_key in initial_elos.keys():
-                self.score_0 = initial_elos[model0_key]
-            else:
-                self.score_0 = 1200
-
-            if model1_key in initial_elos.keys():
-                self.score_1 = initial_elos[model1_key]
-            else:
-                self.score_1 = 1200
-        else:
-            self.initial_elos = {
-                model0_key : 1200,
-                model1_key : 1200
-            }
-            self.score_0 = 1200
-            self.score_1 = 1200
+    def __init__(self):
+        # set the initial scores
+        self.score_0 = 1200
+        self.score_1 = 1200
         self.k = 16
+        self.soft_ceiling=3000
+        self.decay_factor=0.01
+
+    def _soft_ceiling(self, current_rating, rating_change):
+        if current_rating < self.soft_ceiling:
+            # Below the soft ceiling, apply the full rating change
+            new_rating = current_rating + rating_change
+        else:
+            # Above the soft ceiling, apply a diminishing rating change
+            distance_above_ceiling = current_rating - self.soft_ceiling
+            damping_factor = math.exp(-self.decay_factor * distance_above_ceiling)
+            adjusted_change = rating_change * damping_factor
+            new_rating = current_rating + adjusted_change
+        
+        return new_rating
+
 
     def online_elo_update(self, results0 : Dict, results1 : Dict, task_names : List, match_size : int, match_results : Dict):
         index = 0
@@ -45,33 +39,23 @@ class ELO:
         for task_name in task_names:
             match_results[task_name][0].model0_old_elo = self.score_0
             match_results[task_name][0].model1_old_elo = self.score_1
+            answers0[task_name] = []
+            answers1[task_name] = []
             for i in range(0, len(results0["samples"][task_name]), match_size):
-                answers0[task_name] = []
-                answers1[task_name] = []
-                if results0['configs'][task_name]['output_type'] == 'generate_until':
-                    for result0, result1 in zip(results0["samples"][task_name][i:i+match_size], results1["samples"][task_name][i:i+match_size]):
-                        if result0['exact_match'] == 1.0:
-                            answers0[task_name].append(1)
-                        else:
-                            answers0[task_name].append(0)
-                        if result1['exact_match'] == 1.0:
-                            answers1[task_name].append(1)
-                        else:
-                            answers1[task_name].append(0)
-                else:    
-                    for result0, result1 in zip(results0["samples"][task_name][i:i+match_size], results1["samples"][task_name][i:i+match_size]):
-                        nll0 = [response[0][0] for response in result0["resps"]]
-                        nll1 = [response[0][0] for response in result1["resps"]]
-                        prediction0 = argmax(nll0)
-                        prediction1 = argmax(nll1)
-                        if prediction0 == result0["target"]:
-                            answers0[task_name].append(1)
-                        else:
-                            answers0[task_name].append(0)
-                        if prediction1 == result1["target"]:
-                            answers1[task_name].append(1)
-                        else:
-                            answers1[task_name].append(0)
+                for result0, result1 in zip(results0["samples"][task_name][i:i+match_size], results1["samples"][task_name][i:i+match_size]):
+                    nll0 = [response[0][0] for response in result0["resps"]]
+                    nll1 = [response[0][0] for response in result1["resps"]]
+                    prediction0 = argmax(nll0)
+                    prediction1 = argmax(nll1)
+                    if prediction0 == result0["target"]:
+                        answers0[task_name].append(1)
+                    else:
+                        answers0[task_name].append(0)
+                    if prediction1 == result1["target"]:
+                        answers1[task_name].append(1)
+                    else:
+                        answers1[task_name].append(0)
+
                 # calculate the wins, losses, and draws
                 as_0 = []
                 as_1 = []
@@ -88,67 +72,61 @@ class ELO:
                     elif answers0[task_name][i] < answers1[task_name][i]:
                         as_1.append(1)
 
-                expected_score_0 = 1/(1+10**((self.score_0-self.score_1)/400))
-                expected_score_1 = 1/(1+10**((self.score_1-self.score_0)/400))
+                score_diff_0 = min(max(self.score_0 - self.score_1, -MAX_SCORE_DIFF), MAX_SCORE_DIFF)
+                expected_score_0 = 1 / (1 + 10**(score_diff_0 / 400))
+
+                score_diff_1 = min(max(self.score_1 - self.score_0, -MAX_SCORE_DIFF), MAX_SCORE_DIFF)
+                expected_score_1 = 1 / (1 + 10**(score_diff_1 / 400))
 
                 # update Elo
                 if sum(as_0) > sum(as_1):
-                    self.score_0 = self.score_0 + self.k*(1 - expected_score_0)
-                    self.score_1 = self.score_1 + self.k*(0 - expected_score_1)
+                    self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(1 - expected_score_0)), RATING_FLOOR)
+                    self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(0 - expected_score_1)), RATING_FLOOR)
                 elif sum(as_1) > sum(as_0):
-                    self.score_0 = self.score_0 + self.k*(0 - expected_score_0)
-                    self.score_1 = self.score_1 + self.k*(1 - expected_score_1)
+                    self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(0 - expected_score_0)), RATING_FLOOR)
+                    self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(1 - expected_score_1)), RATING_FLOOR)
                 elif sum(as_0) == sum(as_1):
-                    self.score_0 = self.score_0 + self.k*(0.5 - expected_score_0)
-                    self.score_1 = self.score_1 + self.k*(0.5 - expected_score_1)           
+                    self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(0.5 - expected_score_0)), RATING_FLOOR)
+                    self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(0.5 - expected_score_1)), RATING_FLOOR)
                 match_results[task_name][index].model0_old_elo = self.score_0
                 match_results[task_name][index].model1_old_elo = self.score_1
                 index += 1
                 print(f"match {index} : score_0, 1 {self.score_0}, {self.score_1}")
                 print("----------------------------")
-        
-        if self.elo_out is not None:
-            print(f"elo_out file = {self.elo_out}")
-
-            logging.info(f"Writing new ELO score for {self.model0_name}.")
-            self.initial_elos[(self.model0_name, self.model0_bpw)] = self.score_0
-            logging.info(f"Writing new ELO score for {self.model1_name}.")
-            self.initial_elos[(self.model1_name, self.model1_bpw)] = self.score_1
-
-            with open(self.elo_out, 'w') as f:
-                writer = csv.writer(f, delimiter=',')
-                for (k, bpw), v in self.initial_elos.items():
-                    writer.writerow([k, bpw, v])
 
     def offline_elo_update(self, results0, results1, task_names, task_indices):
         # run match
         # sample indices
         # calculate the wins, losses, and draws
+        as_0 = []
         as_1 = []
-        as_2 = []
         for i in task_indices:
             # draw
             if results0[i]['acc'] == results1[i]['acc']:
+                as_0.append(0)
                 as_1.append(0)
-                as_2.append(0)
             # model 1 won 
             elif results0[i]['acc'] > results1[i]['acc']:
-                as_1.append(1)
+                as_0.append(1)
             # model 2 won
             elif results0[i]['acc'] < results1[i]['acc']:
-                as_2.append(1)
-        expected_score_0 = 1/(1+10**((self.score_0-self.score_1)/400))
-        expected_score_1 = 1/(1+10**((self.score_1-self.score_0)/400))
-        
-        # update Elo
-        if sum(as_1) > sum(as_2):
-            self.score_0 = self.score_0 + self.k*(1 - expected_score_0)
-            self.score_1 = self.score_1 + self.k*(0 - expected_score_1)
-        elif sum(as_2) > sum(as_1):
-            self.score_0 = self.score_0 + self.k*(0 - expected_score_0)
-            self.score_1 = self.score_1 + self.k*(1 - expected_score_1)
-        elif sum(as_1) == sum(as_2):
-            self.score_0 = self.score_0 + self.k*(0.5 - expected_score_0)
-            self.score_1 = self.score_1 + self.k*(0.5 - expected_score_1)           
+                as_1.append(1)
+
+            score_diff_0 = min(max(self.score_0 - self.score_1, -MAX_SCORE_DIFF), MAX_SCORE_DIFF)
+            expected_score_0 = 1 / (1 + 10**(score_diff_0 / 400))
+
+            score_diff_1 = min(max(self.score_1 - self.score_0, -MAX_SCORE_DIFF), MAX_SCORE_DIFF)
+            expected_score_1 = 1 / (1 + 10**(score_diff_1 / 400))
+
+            # update Elo
+            if sum(as_0) > sum(as_1):
+                self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(1 - expected_score_0)), RATING_FLOOR)
+                self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(0 - expected_score_1)), RATING_FLOOR)
+            elif sum(as_1) > sum(as_0):
+                self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(0 - expected_score_0)), RATING_FLOOR)
+                self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(1 - expected_score_1)), RATING_FLOOR)
+            elif sum(as_0) == sum(as_1):
+                self.score_0 = max(self._soft_ceiling(self.score_0, self.k*(0.5 - expected_score_0)), RATING_FLOOR)
+                self.score_1 = max(self._soft_ceiling(self.score_1, self.k*(0.5 - expected_score_1)), RATING_FLOOR)      
         print(f"score_0, 1 {self.score_0}, {self.score_1}")
         print("----------------------------")
