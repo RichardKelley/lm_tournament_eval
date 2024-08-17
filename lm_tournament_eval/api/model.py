5d487a9979fa770b6db7e6eb1d6432c6bd6e36d5
Richard Kelley <richard.kelley@gmail.com>
2024-08-16 18:17:13 -0700

    Switched to using HFLM as an external dependency.

--- lm_tournament_eval/api/model.py
+++ lm_tournament_eval/api/model.py
@@ -1,30 +1,12 @@
-import abc
-
-from typing import Dict, List, Optional, Tuple, Type, TypeVar
-
 from lm_tournament_eval import utils
+from typing import Optional, Type, TypeVar
 
-T = TypeVar("T", bound="LM")
+T = TypeVar("T", bound="TournamentLM")
 
-import logging
-
-class LM(abc.ABC):
-
+class TournamentLM:
     def __init__(self) -> None:
-        self._rank = 0 
+        self._rank = 0
         self._world_size = 1
-
-    @abc.abstractmethod
-    def loglikelihood(self, requests) -> List[Tuple[float, bool]]:
-        pass
-
-    @abc.abstractmethod
-    def loglikelihood_rolling(self, requests) -> List[Tuple[float]]:
-        pass
-
-    @abc.abstractmethod
-    def generate_until(self, requests) -> List[str]:
-        pass
 
     @property
     def rank(self):
--- lm_tournament_eval/api/registry.py
+++ lm_tournament_eval/api/registry.py
@@ -3,7 +3,7 @@ from typing import Callable, Dict
 
 import evaluate as hf_evaluate
 
-from lm_tournament_eval.api.model import LM
+from lm_tournament_eval.api.model import TournamentLM
 
 eval_logger = logging.getLogger("lm-eval")
 
@@ -17,7 +17,7 @@ def register_model(*names):
     def decorate(cls):
         for name in names:
             assert issubclass(
-                cls, LM
+                cls, TournamentLM
             ), f"Model '{name}' ({cls.__name__}) must extend LM class"
 
             assert (
