3ff33a7835f56c85c5860a5e527362b0b38e6d77
Richard Kelley <richard.kelley@gmail.com>
2024-08-20 10:03:09 -0700

    Simplified model code using updated hflm (v0.0.5).

--- lm_tournament_eval/api/registry.py
+++ lm_tournament_eval/api/registry.py
@@ -3,7 +3,7 @@ from typing import Callable, Dict
 
 import evaluate as hf_evaluate
 
-from lm_tournament_eval.api.model import TournamentLM
+from hflm import LM
 
 eval_logger = logging.getLogger("lm-eval")
 
@@ -17,7 +17,7 @@ def register_model(*names):
     def decorate(cls):
         for name in names:
             assert issubclass(
-                cls, TournamentLM
+                cls, LM
             ), f"Model '{name}' ({cls.__name__}) must extend LM class"
 
             assert (
--- lm_tournament_eval/models/huggingface_model.py
+++ lm_tournament_eval/models/huggingface_model.py
@@ -1,50 +1,23 @@
-from lm_tournament_eval.api.model import TournamentLM
 from hflm import HFLM
 
-from typing import List, Tuple, Union, Optional
+from typing import List, Tuple
 
 from lm_tournament_eval.api.registry import register_model
 
 @register_model("hf-auto", "hf", "huggingface")
-class TournamentHFLM(TournamentLM, HFLM):
+class TournamentHFLM(HFLM):
     def __init__(self,
         model,
         *args,
         **kwargs,                                  
     ):
-        TournamentLM.__init__(self)
-        HFLM.__init__(self, model, *args, **kwargs)
+        super().__init__(model, *args, **kwargs)
 
     def loglikelihood(self, requests: List[Tuple[str,str]], disable_tqdm: bool = False) -> List[Tuple[float, bool]]:
-         return HFLM.loglikelihood(self, [req.args for req in requests], disable_tqdm=disable_tqdm)
+        return super().loglikelihood([req.args for req in requests], disable_tqdm=disable_tqdm)
     
     def loglikelihood_rolling(self, requests : List[str], disable_tqdm : bool = False) -> List[Tuple[float]]:
-        return HFLM.loglikelihood_rolling(self, [req.args for req in requests], disable_tqdm=disable_tqdm)
+        return super().loglikelihood_rolling([req.args for req in requests], disable_tqdm=disable_tqdm)
 
     def generate_until(self, requests : List[Tuple[str, dict]], disable_tqdm : bool = False) -> List[str]:
-        return HFLM.generate_until(self, [req.args for req in requests], disable_tqdm=disable_tqdm)
-    
-    def get_model_info(self) -> dict:
-        """
-        Method to get Hugging Face model information for experiment reproducibility.
-        """
-
-        def get_model_num_params(model) -> int:
-            if hasattr(model, "num_parameters"):
-                return model.num_parameters()
-            if hasattr(model, "parameters"):
-                return sum(p.numel() for p in model.parameters())
-            else:
-                return -1
-
-        def get_model_dtype(model) -> str:
-            if hasattr(model, "dtype"):
-                return model.dtype
-            else:
-                return ""
-
-        model_info = {
-            "model_num_parameters": get_model_num_params(self._model),
-            "model_dtype": get_model_dtype(self._model),
-        }
-        return model_info        
+        return super().generate_until([req.args for req in requests], disable_tqdm=disable_tqdm)
