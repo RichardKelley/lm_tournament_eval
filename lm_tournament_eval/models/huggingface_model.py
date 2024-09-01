2c1e73cdfa44d1c6386dd8eb6a2396be2e3c5956
Richard Kelley <richard.kelley@gmail.com>
2024-08-31 23:17:46 -0700

    Added an index into array for loglikelihood_rolling.

--- lm_tournament_eval/models/huggingface_model.py
+++ lm_tournament_eval/models/huggingface_model.py
@@ -4,6 +4,8 @@ from typing import List, Tuple
 
 from lm_tournament_eval.api.registry import register_model
 
+import logging
+
 @register_model("hf-auto", "hf", "huggingface")
 class TournamentHFLM(HFLM):
     def __init__(self,
@@ -17,7 +19,7 @@ class TournamentHFLM(HFLM):
         return super().loglikelihood([req.args for req in requests], disable_tqdm=disable_tqdm)
     
     def loglikelihood_rolling(self, requests : List[str], disable_tqdm : bool = False) -> List[Tuple[float]]:
-        return super().loglikelihood_rolling([req.args for req in requests], disable_tqdm=disable_tqdm)
+        return super().loglikelihood_rolling([req.args[0] for req in requests], disable_tqdm=disable_tqdm)
 
     def generate_until(self, requests : List[Tuple[str, dict]], disable_tqdm : bool = False) -> List[str]:
         return super().generate_until([req.args for req in requests], disable_tqdm=disable_tqdm)
