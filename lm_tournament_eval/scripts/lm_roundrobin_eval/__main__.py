a0fd0296399e00b2516ca1b7daf0c010d5c92de2
Richard Kelley <richard.kelley@gmail.com>
2024-09-03 15:33:00 -0700

    Added support for roundrobin evaluation.

---
+++ lm_tournament_eval/scripts/lm_roundrobin_eval/__main__.py
@@ -0,0 +1,106 @@
+import argparse
+import json
+import sys
+import logging
+import datetime
+import os
+import csv
+
+from lm_tournament_eval import utils
+from lm_tournament_eval.api.tournament import TournamentConfig, Tournament
+from lm_tournament_eval.api.offline_tournament import OfflineTournamentConfig, OfflineTournament
+from lm_tournament_eval.api.task import TaskConfig
+from lm_tournament_eval.tasks import TaskManager
+from lm_tournament_eval.evaluator_utils import request_caching_arg_to_dict
+from lm_tournament_eval.api.scheduler import FileScheduler, SamplingScheduler, DefaultScheduler
+
+from lm_tournament_eval.score_database import ScoreDatabase
+
+from lm_tournament_eval.scripts.script_args import (
+    setup_parser,
+    validate_tasks,
+    setup_scheduler,
+    setup_trust_remote_code,
+    setup_filter_list,
+    setup_roundrobin_models
+)
+
+def get_roundrobin_schedule(num_models):
+    players = list(range(num_models))
+    if num_models % 2 != 0:
+        players.append(None)  # Add a dummy player if num_models is odd
+    
+    n = len(players)
+    matches = []
+    
+    for round in range(n - 1):
+        round_matches = []
+        for i in range(n // 2):
+            if players[i] is not None and players[n - 1 - i] is not None:
+                round_matches.append((players[i], players[n - 1 - i]))
+        matches.extend(round_matches)
+        
+        players = [players[0]] + [players[-1]] + players[1:-1]
+    
+    return matches
+
+
+def run_roundrobin_eval():
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    task_manager = TaskManager(args.verbosity, include_path=args.include_path)
+    task_names = validate_tasks(args, task_manager)
+    args = setup_trust_remote_code(args)
+
+    db = ScoreDatabase(args.db_path)
+
+    filter_list = setup_filter_list(args=args, task_names=task_names)
+    scheduler = setup_scheduler(args)
+
+    model_list = setup_roundrobin_models(args)
+
+    tournament_idxs = get_roundrobin_schedule(len(model_list))
+    tournament_list = [(model_list[i], model_list[j]) for (i,j) in tournament_idxs]
+
+    for t in tournament_list:
+        model0 = t[0]
+        model1 = t[1]
+
+        tournament_name = f'{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.{model0[0]}.{model1[0]}'
+
+        logging.info(f"Running tournament {tournament_name}")
+
+        cfg = TournamentConfig(name=tournament_name,
+                              rounds=args.num_rounds,
+                              model0_name=model0[0],
+                              model0_args=model0[1],
+                              model1_name=model1[0],
+                              model1_args=model1[1],
+                              task_names=task_names,
+                              batch_size=args.batch_size,
+                              gen_kwargs=args.gen_kwargs,
+                              device=args.device,
+                              limit=args.limit,
+                              match_size=args.match_size,
+                              cmd_filter=filter_list,
+                              random_seed=args.random_seed,
+                              numpy_random_seed=args.numpy_random_seed,
+                              torch_random_seed=args.torch_random_seed,
+                              fewshot_random_seed=args.fewshot_random_seed
+                             )
+        
+        tournament = Tournament(
+            cfg, 
+            task_names, 
+            task_manager, 
+            args.verbosity, 
+            scheduler,
+            db=db)
+
+        db.record_tournament(tournament)
+
+        tournament.run_tournament()
+
+if __name__ == '__main__':
+    run_roundrobin_eval()
---
+++ lm_tournament_eval/scripts/lm_tournament_eval/__main__.py
@@ -0,0 +1,98 @@
+import argparse
+import json
+import sys
+import logging
+import datetime
+import os
+import csv
+
+from lm_tournament_eval import utils
+from lm_tournament_eval.api.tournament import TournamentConfig, Tournament
+from lm_tournament_eval.api.offline_tournament import OfflineTournamentConfig, OfflineTournament
+from lm_tournament_eval.api.task import TaskConfig
+from lm_tournament_eval.tasks import TaskManager
+from lm_tournament_eval.evaluator_utils import request_caching_arg_to_dict
+from lm_tournament_eval.api.scheduler import FileScheduler, SamplingScheduler, DefaultScheduler
+
+from lm_tournament_eval.score_database import ScoreDatabase
+
+from lm_tournament_eval.scripts.script_args import (
+    setup_parser,
+    validate_tasks,
+    setup_scheduler,
+    setup_trust_remote_code,
+    setup_filter_list
+)
+
+def run_tournament():
+
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    task_manager = TaskManager(args.verbosity, include_path=args.include_path)
+    task_names = validate_tasks(args, task_manager)
+    args = setup_trust_remote_code(args)
+
+    db = ScoreDatabase(args.db_path)
+
+    filter_list = setup_filter_list(args=args, task_names=task_names)
+    scheduler = setup_scheduler(args)
+
+    tournament_name = f'{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.{args.model0}.{args.model1}'
+
+    # set up wandb logger.
+
+    if args.offline == True:
+        # validate tournament parameters.
+        task_config = TaskConfig()
+        cfg = OfflineTournamentConfig(name=tournament_name,
+                                      offline_file_0=args.offline_file_0,
+                                      offline_file_1=args.offline_file_1,
+                                      task_name=args.tasks,
+                                      rounds=args.num_rounds,
+                                      num_samples=args.match_size,
+                                      task_config=task_config,
+                                      model0_name = args.model0,
+                                      model1_name = args.model1
+                                     )
+        # create offline tournament
+        tournament = OfflineTournament(cfg)
+
+        # run tournament evaluator.
+        result = tournament.run_tournament()    
+    else:
+        # validate tournament parameters.
+        cfg = TournamentConfig(name=tournament_name,
+                              rounds=args.num_rounds,
+                              model0_name=args.model0,
+                              model0_args=args.model0_args,
+                              model1_name=args.model1,
+                              model1_args=args.model1_args,
+                              task_names=task_names,
+                              batch_size=args.batch_size,
+                              gen_kwargs=args.gen_kwargs,
+                              device=args.device,
+                              limit=args.limit,
+                              match_size=args.match_size,
+                              cmd_filter=filter_list,
+                              random_seed=args.random_seed,
+                              numpy_random_seed=args.numpy_random_seed,
+                              torch_random_seed=args.torch_random_seed,
+                              fewshot_random_seed=args.fewshot_random_seed
+                             )
+
+        tournament = Tournament(
+            cfg, 
+            task_names, 
+            task_manager, 
+            args.verbosity, 
+            scheduler,
+            db=db)
+
+        db.record_tournament(tournament)
+
+        tournament.run_tournament()
+
+
+if __name__ == "__main__":
+    run_tournament()
